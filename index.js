2d18796c30890ed87b23265c6b3fe8d148980a8e
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-02-03 15:10:50 -0500

    refactorice el codigo de js 2

--- index.js
+++ index.js
@@ -16,75 +16,97 @@ let selected3 = false;
 let selected4 = false;
 let selected5 = false;
 
+const MOUSEOUT_STYLES = `
+
+opacity: 0.4;
+background: #262f38;
+cursor: pointer;
+font-weight: bold;
+
+width: calc(1.5em + 1.5vw);
+height: calc(1.5em + 1.5vw);
+border-radius: 1000rem;
+
+display: flex;
+justifyContent: center;
+alignItems: center;`;
+
 const number1Selected = () => {
   selected1 = true;
+  selected2 = false;
+  selected3 = false;
+  selected4 = false;
+  selected5 = false;
 
-  number1.style.transition = "0.3s";
-
-  number1.style.opacity = "1";
-  number1.style.background = "gray";
-  number2.style.background = "#262F38";
-  number3.style.background = "#262F38";
-  number4.style.background = "#262F38";
-  number5.style.background = "#262F38";
+  number1.style.cssText = "transition: 0.3s; opacity: 1; background: gray";
+  number2.style.cssText = "background: #262F38; opacity: 0.4;";
+  number3.style.cssText = "background: #262F38; opacity: 0.4;";
+  number4.style.cssText = "background: #262F38; opacity: 0.4;";
+  number5.style.cssText = "background: #262F38; opacity: 0.4;";
 
   view2Text1.textContent = "You selected 1 out 5";
 };
 
 const number2Selected = () => {
+  selected1 = false;
   selected2 = true;
+  selected3 = false;
+  selected4 = false;
+  selected5 = false;
 
-  number2.style.transition = "0.3s";
-
-  number1.style.background = "#262F38";
-  number2.style.background = "gray";
-  number3.style.background = "#262F38";
-  number4.style.background = "#262F38";
-  number5.style.background = "#262F38";
+  number1.style.cssText = "background: #262F38; opacity: 0.4;";
+  number2.style.cssText = "transition: 0.3s; opacity: 1; background: gray";
+  number3.style.cssText = "background: #262F38; opacity: 0.4;";
+  number4.style.cssText = "background: #262F38; opacity: 0.4;";
+  number5.style.cssText = "background: #262F38; opacity: 0.4;";
 
   view2Text1.textContent = "You selected 2 out 5";
 };
 
 const number3Selected = () => {
+  selected1 = false;
+  selected2 = false;
   selected3 = true;
+  selected4 = false;
+  selected5 = false;
 
-  number3.style.transition = "0.3s";
-
-  number1.style.background = "#262F38";
-  number2.style.background = "#262F38";
-  number3.style.background = "gray";
-  number4.style.background = "#262F38";
-  number5.style.background = "#262F38";
+  number1.style.cssText = "background: #262F38; opacity: 0.4;";
+  number2.style.cssText = "background: #262F38; opacity: 0.4;";
+  number3.style.cssText = "transition: 0.3s; opacity: 1; background: gray";
+  number4.style.cssText = "background: #262F38; opacity: 0.4;";
+  number5.style.cssText = "background: #262F38; opacity: 0.4;";
 
   view2Text1.textContent = "You selected 3 out 5";
 };
 
 const number4Selected = () => {
+  selected1 = false;
+  selected2 = false;
+  selected3 = false;
   selected4 = true;
+  selected5 = false;
 
-  number4.style.transition = "0.3s";
-
-  number1.style.background = "#262F38";
-  number2.style.background = "#262F38";
-  number3.style.background = "#262F38";
-  number4.style.background = "gray";
-  number5.style.background = "#262F38";
+  number1.style.cssText = "background: #262F38; opacity: 0.4;";
+  number2.style.cssText = "background: #262F38; opacity: 0.4;";
+  number3.style.cssText = "background: #262F38; opacity: 0.4;";
+  number4.style.cssText = "transition: 0.3s; opacity: 1; background: gray";
+  number5.style.cssText = "background: #262F38; opacity: 0.4;";
 
   view2Text1.textContent = "You selected 4 out 5";
 };
 
 const number5Selected = () => {
+  selected1 = false;
+  selected2 = false;
+  selected3 = false;
+  selected4 = false;
   selected5 = true;
 
-  number5.style.transition = "0.3s";
-
-  number1.style.background = "#262F38";
-  number2.style.background = "#262F38";
-  number3.style.background = "#262F38";
-  number4.style.background = "#262F38";
-  number5.style.background = "gray";
-
-  view2Text1.textContent = "You selected 5 out 5";
+  number1.style.cssText = "background: #262F38; opacity: 0.4;";
+  number2.style.cssText = "background: #262F38; opacity: 0.4;";
+  number3.style.cssText = "background: #262F38; opacity: 0.4;";
+  number4.style.cssText = "background: #262F38; opacity: 0.4;";
+  number5.style.cssText = "transition: 0.3s; opacity: 1; background: gray";
 };
 
 const handleClickSelect = (e) => {
@@ -129,20 +151,15 @@ document.addEventListener("click", (e) => {
 
 document.addEventListener("mouseover", (e) => {
   if (e.target.matches("#number1")) {
-    number1.style.background = "#ff7512";
-    number1.style.opacity = "1";
+    number1.style.cssText = "background :#ff7512; opacity: 1";
   } else if (e.target.matches("#number2")) {
-    number2.style.background = "#ff7512";
-    number2.style.opacity = "1";
+    number2.style.cssText = "background :#ff7512; opacity: 1";
   } else if (e.target.matches("#number3")) {
-    number3.style.background = "#ff7512";
-    number3.style.opacity = "1";
+    number3.style.cssText = "background :#ff7512; opacity: 1";
   } else if (e.target.matches("#number4")) {
-    number4.style.background = "#ff7512";
-    number4.style.opacity = "1";
+    number4.style.cssText = "background :#ff7512; opacity: 1";
   } else if (e.target.matches("#number5")) {
-    number5.style.background = "#ff7512";
-    number5.style.opacity = "1";
+    number5.style.cssText = "background :#ff7512; opacity: 1";
   }
 });
 
@@ -151,41 +168,17 @@ document.addEventListener("mouseout", (e) => {
     if (selected1) {
       number1Selected();
     } else {
-      number1.style.transition = "0.4s";
-
-      number1.style.background = "#262f38";
-      number1.style.cursor = "pointer";
-      number1.style.fontWeight = "bold";
-      number1.style.opacity = "0.4";
-
-      number1.style.width = "calc(1.5em + 1.5vw)";
-      number1.style.height = "calc(1.5em + 1.5vw)";
-      number1.style.borderRadius = "1000rem";
-
-      number1.style.display = "flex";
-      number1.style.justifyContent = "center";
-      number1.style.alignItems = "center";
+      number1.style.cssText = `
+      
+       `;
 
       view2Text1.textContent = "You selected 1 out 5";
     }
   } else if (e.target.matches("#number2")) {
-    if (selected1) {
+    if (selected2) {
       number2Selected();
     } else {
-      number2.style.transition = "0.4s";
-
-      number2.style.background = "#262f38";
-      number2.style.cursor = "pointer";
-      number2.style.fontWeight = "bold";
-      number2.style.opacity = "0.4";
-
-      number2.style.width = "calc(1.5em + 1.5vw)";
-      number2.style.height = "calc(1.5em + 1.5vw)";
-      number2.style.borderRadius = "1000rem";
-
-      number2.style.display = "flex";
-      number2.style.justifyContent = "center";
-      number2.style.alignItems = "center";
+      number2.style.cssText = MOUSEOUT_STYLES;
 
       view2Text1.textContent = "You selected 1 out 5";
     }
@@ -193,68 +186,32 @@ document.addEventListener("mouseout", (e) => {
     if (selected3) {
       number3Selected();
     } else {
-      number3.style.transition = "0.4s";
-
-      number3.style.background = "#262f38";
-      number3.style.cursor = "pointer";
-      number3.style.fontWeight = "bold";
-      number3.style.opacity = "0.4";
-
-      number3.style.width = "calc(1.5em + 1.5vw)";
-      number3.style.height = "calc(1.5em + 1.5vw)";
-      number3.style.borderRadius = "1000rem";
-
-      number3.style.display = "flex";
-      number3.style.justifyContent = "center";
-      number3.style.alignItems = "center";
+      number3.style.cssText = MOUSEOUT_STYLES;
     }
   } else if (e.target.matches("#number4")) {
     if (selected4) {
       number4Selected();
     } else {
-      number4.style.transition = "0.4s";
-
-      number4.style.background = "#262f38";
-      number4.style.cursor = "pointer";
-      number4.style.fontWeight = "bold";
-      number4.style.opacity = "0.4";
-
-      number4.style.width = "calc(1.5em + 1.5vw)";
-      number4.style.height = "calc(1.5em + 1.5vw)";
-      number4.style.borderRadius = "1000rem";
-
-      number4.style.display = "flex";
-      number4.style.justifyContent = "center";
-      number4.style.alignItems = "center";
+      number4.style.cssText = MOUSEOUT_STYLES;
     }
   } else if (e.target.matches("#number5")) {
     if (selected5) {
       number5Selected();
     } else {
-      number5.style.transition = "0.4s";
-
-      number5.style.background = "#262f38";
-      number5.style.cursor = "pointer";
-      number5.style.fontWeight = "bold";
-      number5.style.opacity = "0.4";
-
-      number5.style.width = "calc(1.5em + 1.5vw)";
-      number5.style.height = "calc(1.5em + 1.5vw)";
-      number5.style.borderRadius = "1000rem";
-
-      number5.style.display = "flex";
-      number5.style.justifyContent = "center";
-      number5.style.alignItems = "center";
+      number5.style.cssText = MOUSEOUT_STYLES;
     }
   }
 });
 
 const handleSubmit = () => {
   view1.style.display = "none";
-  view2.style.display = "flex";
-  view2.style.justifyContent = "center";
-  view2.style.alignItems = "center";
-  view2.style.flexWrap = "wrap";
+
+  view2.style.cssText = `
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;`;
 
   main.className = "animate__animated animate__backInDown";
 };
